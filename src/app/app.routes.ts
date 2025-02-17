7bb1cdb4c09fad9f785473ecc6ed6179d2e15833
sinedied <noda@free.fr>
2025-02-17 09:44:58 +0100

    chore: update defaut route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'repos', pathMatch: 'full' },
   {
-    path: 'repos',
+    path: '',
     loadComponent: () => import('./repos.component.js').then(m => m.ReposComponent)
   }
 ];
